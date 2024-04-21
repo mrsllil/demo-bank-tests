682a9ce60fa2f319230c701c6fafd50c015c914b
mrsllil <klaudia9511@gmail.com>
2024-04-21 21:35:50 +0200

    added before each to pulpit

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,28 +3,24 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
     const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = '5454553r';
+
     await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
   });
 
   test('quick payment with correct data', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
-    const userPassword = '5454553r';
-
     const receiverId = '2';
     const transferAmount = '150';
     const transferTitle = 'pizza';
     const expectedTransferReceiver = 'Chuck Demobankowy';
-
-    // Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
     const expectedMessage = `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`;
 
-    // wait for page to fully load:
-    await page.waitForLoadState('domcontentloaded');
-
+    // Act
     await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
@@ -37,20 +33,10 @@ test.describe('Pulpit tests', () => {
   });
   test('succesfull mobile top-up', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
-    const userPassword = '5454553r';
     const transferAmount = '70';
     const receiverNumber = '500 xxx xxx';
 
     // Act
-
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
-    // wait for page to fully load:
-    await page.waitForLoadState('domcontentloaded');
-
     await page.locator('#widget_1_topup_receiver').selectOption(receiverNumber);
     await page.locator('#widget_1_topup_amount').fill(transferAmount);
     await page.locator('#uniform-widget_1_topup_agreement').click();
