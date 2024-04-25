a42c28c18f5340c511ee09cc17c717df1de4e6f5
mrsllil <klaudia9511@gmail.com>
2024-04-25 21:35:27 +0200

    payment test (part 2)

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -10,22 +10,24 @@ test.describe('Payment tests', () => {
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
+    await page.getByRole('link', { name: 'płatności' }).click();
   });
 
   test('simple payment', async ({ page }) => {
-    await page.getByRole('link', { name: 'płatności' }).click();
-    await page.getByTestId('transfer_receiver').click();
-    await page.getByTestId('transfer_receiver').fill('Jan Nowak');
-    await page.getByTestId('form_account_to').click();
-    await page
-      .getByTestId('form_account_to')
-      .fill('12 3456 7891 2345 6789 0000 00');
-    await page.getByTestId('form_amount').click();
-    await page.getByTestId('form_amount').fill('222');
+    //Arrange
+    const transferReceiver = 'Jan Nowak';
+    const transferAccount = '12 3456 7891 2345 6789 0000';
+    const transferAmount = '222';
+    const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla Jan Nowak`;
+
+    // Act
+    await page.getByTestId('transfer_receiver').fill(transferReceiver);
+    await page.getByTestId('form_amount').fill(transferAmount);
+    await page.getByTestId('form_account_to').fill(transferAccount);
     await page.getByRole('button', { name: 'wykonaj przelew' }).click();
     await page.getByTestId('close-button').click();
-    await page
-      .getByRole('link', { name: 'Przelew wykonany! 222,00PLN' })
-      .click();
+
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
 });
